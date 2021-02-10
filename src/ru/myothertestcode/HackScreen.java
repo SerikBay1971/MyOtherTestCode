48c1a965c1ef5002afe920d5c737fd158d2a6cce
SerikBay1971 <serg.kolosov@gmail.com>
2021-02-10 13:25:00 +0400

    Переписал. Заменил throws на try-catch

--- src/ru/myothertestcode/HackScreen.java
+++ src/ru/myothertestcode/HackScreen.java
@@ -9,17 +9,30 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class HackScreen {
-    public static void main(String[] args) throws IOException, AWTException, InterruptedException {
+    public static void main(String[] args) {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd_HHmmss");
         String nowFormat;
         boolean infinity = true;
 
         while (infinity) {
-            Thread.sleep(15 * 60 * 1000); // каждые 15 минут
-            BufferedImage image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+            BufferedImage image = null;
+            try {
+                image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+            } catch (AWTException e) {
+                e.printStackTrace();
+            }
             Date now = new Date();
             nowFormat = "d:/ScreenFiles/" + formatter.format(now) + ".png";
-            ImageIO.write(image, "png", new File(nowFormat));
+            try {
+                ImageIO.write(image, "png", new File(nowFormat));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                Thread.sleep(15 * 1000); // каждые 15 секунд
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
